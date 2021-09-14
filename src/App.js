2cb10fd95a99064ac4a181c8703d6e0a2bb64259
shreyas girjapure <shreyasgirjapure1122@gmail.com>
2021-09-14 20:47:14 +0530

    removed unused vars

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Header from "./Components/Header.js";
 import Footer from "./Components/Footer.js";
-import SignUp from "./Components/SignUp.js";
-import { Router } from "react-router";
+
 
 function App() {
   return (
